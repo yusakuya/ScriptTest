4500bc3bfa261595c82117fb48f0430916c3152f
yusakuya <yu.sakuya24@gmail.com>
2017-04-25 21:13:09 +0900

    ScriptTest
    C#テスト

---
+++ Assets/Test1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+        //順に表示する
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
+        }
+        //逆順に表示する
+        for (int i = 4; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Test2.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss
+{
+    private int mp = 53;
+
+    // 魔法攻撃用の関数
+    public void Magic()
+    {
+        if (mp >= 5)
+        {
+            this.mp -= 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");
+        }
+        else
+        {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    }
+}
+
+public class Test2 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Boss lastBoss = new Boss();
+        lastBoss.Magic();
+        lastBoss.Magic();
+        lastBoss.Magic();
+        lastBoss.Magic();
+        lastBoss.Magic();
+        lastBoss.Magic();
+        lastBoss.Magic();
+        lastBoss.Magic();
+        lastBoss.Magic();
+        lastBoss.Magic();
+        lastBoss.Magic();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
